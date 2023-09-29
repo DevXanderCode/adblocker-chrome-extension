06fd36fddbb231fc59f036ba9aa6f78485de18aa
Alex Nweke <nwekealexchinedu@gmail.com>
2023-09-29 02:58:46 +0100

    block ad using content script

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,1 +1,25 @@
-// TODO: content script
+const rules: {
+  [url: string]: () => void;
+} = {
+  "https://www.nytimes.com/section/technology": filterNYTechnology2,
+};
+
+function filterNYTechnology() {
+  const app = document.querySelector("#site-content");
+  const wrapper = document.querySelector("top-wrapper");
+  app.removeChild(wrapper);
+}
+
+function filterNYTechnology2() {
+  const divs = document.getElementsByTagName("div");
+  for (const div of divs) {
+    if (div.className.indexOf("ad") != -1) {
+      div.style.display = "none";
+    }
+  }
+}
+
+if (document.URL in rules) {
+  console.log("document url", document.URL);
+  rules[document.URL]();
+}
